a70c48674baa2dbf2301bc19afa639016aeffbd3
oldark87 <oldark87@gmail.com>
2014-02-15 15:26:59 -0500

    added verification into AndroidLoginManager.java

--- src/main/java/cs2340/woms/auth/AndroidLoginManager.java
+++ src/main/java/cs2340/woms/auth/AndroidLoginManager.java
@@ -1,9 +1,12 @@
 package cs2340.woms.auth;
 
+import java.util.ArrayList;
+
 public class AndroidLoginManager implements LoginManager {
 
     public static LoginManager instance = new AndroidLoginManager();
-
+    private ArrayList<Pair> names;
+    
     private AndroidLoginManager() { }
 
     @Override
@@ -15,7 +18,36 @@ public class AndroidLoginManager implements LoginManager {
         if (username.equals("admin") && password.equals("pass123")) {
             return true;
         }
-
+        for(Pair pair: names){
+        	if(pair.getName() == username){
+        		if(pair.getPass() == password){
+        			return true;
+        		}
+        		else return false;
+        	}
+        }
+        
         return false;
     }
+    
+    public void register(String name, String pass){
+    	Pair pair = new Pair(name, pass);
+    	names.add(pair);
+    }
+    
+    private class Pair{
+    	private String name, pass;
+    	
+    	public Pair(String name, String pass){
+    		this.name = name;
+    		this.pass = pass;
+    	}
+    	public String getName(){
+    		return name;
+    	}
+    	public String getPass(){
+        	return pass;
+    	}
+    }
 }
+
