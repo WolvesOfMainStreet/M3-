15b8d8a7b9eecf47c8181be5b22397e79ee952d9
Devon Cooper <dnoved1@aim.com>
2014-02-19 13:36:53 -0500

    Created basic FinanceAccount class
    At the moment it only holds a name and a balance. Future versions may
    add additional information and methods to for example calculate reports.

---
+++ src/main/java/cs2340/woms/account/FinanceAccount.java
@@ -0,0 +1,35 @@
+package cs2340.woms.account;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+/**
+ * A financial account which stores information such as transactions and which
+ * can generate reports based on them. Not to be confused with LoginAccounts,
+ * which are used for logging into the application and which can have any number
+ * of FinanceAccounts.
+ */
+public class FinanceAccount {
+
+    private final String name;
+    private final BigDecimal balance;
+
+    /**
+     * Creates a new FinanceAccount with the given name and a starting balance
+     * of 0.
+     *
+     * @param name the full name for this FinanceAccount.
+     */
+    public FinanceAccount(String name) {
+        this.name = name;
+        this.balance = new BigDecimal(0, MathContext.DECIMAL32);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance.doubleValue();
+    }
+}
