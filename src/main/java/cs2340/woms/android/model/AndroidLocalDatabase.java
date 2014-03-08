0eab95dd88eed4d02b24a288fefd1be3cda92ca4
Devon Cooper <dnoved1@aim.com>
2014-03-08 14:05:04 -0500

    Added support User/Account serialization in SQL

--- src/main/java/cs2340/woms/android/model/AndroidLocalDatabase.java
+++ src/main/java/cs2340/woms/android/model/AndroidLocalDatabase.java
@@ -1,6 +1,5 @@
 package cs2340.woms.android.model;
 
-import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,12 +15,16 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import cs2340.woms.model.Account;
-import cs2340.woms.model.User;
 import cs2340.woms.model.Transaction;
+import cs2340.woms.model.User;
 
 public final class AndroidLocalDatabase extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 3;
+    // TODO: make column creation dynamic based on frequency of key use.
+    // I.e if lots of accounts start using key X, make a new column for X
+    // rather than saving it under extras.
+
+    private static final int DATABASE_VERSION = 4;
     private static final String DATABASE_NAME = "womsMainDB";
 
     private static AndroidLocalDatabase instance;
@@ -33,12 +36,16 @@ public final class AndroidLocalDatabase extends SQLiteOpenHelper {
     private static final String TABLE_TRANSACTION = "transactions";
 
     private static final String KEY_USER_PK = "user_id";
+    private static final String KEY_USER_CLASS = "class";
     private static final String KEY_USER_USERNAME = "username";
     private static final String KEY_USER_PASSWORD = "password";
+    private static final String KEY_USER_EXTRAS = "extras";
 
     private static final String KEY_ACCOUNT_PK = "account_id";
+    private static final String KEY_ACCOUNT_CLASS = "class";
     private static final String KEY_ACCOUNT_NAME = "name";
     private static final String KEY_ACCOUNT_BALANCE = "balance";
+    private static final String KEY_ACCOUNT_EXTRAS = "extras";
 
     private static final String KEY_TRANSACTION_PK = "transaction_id";
     private static final String KEY_TRANSACTION_AMOUNT = "amount";
@@ -52,16 +59,20 @@ public final class AndroidLocalDatabase extends SQLiteOpenHelper {
     private static final String CREATE_TABLE_USER =
             "CREATE TABLE " + TABLE_USER + "("
                 + KEY_USER_PK + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + KEY_USER_CLASS + " TEXT NOT NULL,"
                 + KEY_USER_USERNAME + " TEXT UNIQUE NOT NULL,"
-                + KEY_USER_PASSWORD + " TEXT NOT NULL"
+                + KEY_USER_PASSWORD + " TEXT NOT NULL,"
+                + KEY_USER_EXTRAS + " TEXT NOT NULL"
             + ")";
 
     private static final String CREATE_TABLE_ACCOUNT =
             "CREATE TABLE " + TABLE_ACCOUNT + "("
                 + KEY_ACCOUNT_PK + " INTEGER PRIMARY KEY AUTOINCREMENT,"
                 + KEY_USER_PK + " INTEGER,"
+                + KEY_ACCOUNT_CLASS + " TEXT NOT NULL,"
                 + KEY_ACCOUNT_NAME + " TEXT NOT NULL,"
                 + KEY_ACCOUNT_BALANCE + " TEXT NOT NULL,"
+                + KEY_ACCOUNT_EXTRAS + " TEXT NOT NULL,"
                 + "FOREIGN KEY (" + KEY_USER_PK + ") REFERENCES " + TABLE_USER + "(" + KEY_USER_PK + ")"
             + ")";
 
@@ -117,14 +128,44 @@ public final class AndroidLocalDatabase extends SQLiteOpenHelper {
         return getUserID(user) != -1;
     }
 
-    public boolean addUser(String username, String password) {
+    public boolean addUser(User user) {
         SQLiteDatabase db = this.getWritableDatabase();
 
+        //-----Get the serializable data for the user---------------------------
+        Map<String, String> writeData = new HashMap<String, String>();
+        writeData = user.write(writeData);
+
+        //-----Get the standard user data, with appropriate defaults------------
+        String username = writeData.remove(User.SAVE_KEY_USERNAME);
+        if (username == null) {
+            System.err.println("Error getting username from saved user data.");
+            username = "unknown";
+        }
+
+        String password = writeData.remove(User.SAVE_KEY_PASSWORD);
+        if (password == null) {
+            System.err.println("Error getting password from saved user data.");
+            password = "pass123";
+        }
+
+        //-----Get the extra user data------------------------------------------
+        StringBuilder extras = new StringBuilder();
+        for (Entry<String, String> e: writeData.entrySet()) {
+            extras.append(e.getKey());
+            extras.append(":");
+            extras.append(e.getValue());
+            extras.append("|");
+        }
+
+        //-----Write all data to sql tables-------------------------------------
         ContentValues cv = new ContentValues();
+        cv.put(KEY_USER_CLASS, user.getClass().getName());
         cv.put(KEY_USER_USERNAME, username);
         cv.put(KEY_USER_PASSWORD, password);
+        cv.put(KEY_USER_EXTRAS, extras.toString());
 
         long rowID = db.insert(TABLE_USER, null, cv);
+
         return rowID != -1;
     }
 
@@ -136,10 +177,39 @@ public final class AndroidLocalDatabase extends SQLiteOpenHelper {
 
         SQLiteDatabase db = this.getWritableDatabase();
 
+        //-----Get the serializable data for the account------------------------
+        Map<String, String> writeData = new HashMap<String, String>();
+        writeData = account.write(writeData);
+
+        //-----Get the standard account data, with appropriate defaults---------
+        String name = writeData.remove(Account.SAVE_KEY_NAME);
+        if (name == null) {
+            System.err.println("Error getting name from saved account data.");
+            name = "Unknown";
+        }
+
+        String balance = writeData.remove(Account.SAVE_KEY_BALANCE);
+        if (balance == null) {
+            System.err.println("Error getting balance from saved account data.");
+            balance = "0";
+        }
+
+        //-----Get the extra account data---------------------------------------
+        StringBuilder extras = new StringBuilder();
+        for (Entry<String, String> e: writeData.entrySet()) {
+            extras.append(e.getKey());
+            extras.append(":");
+            extras.append(e.getValue());
+            extras.append("|");
+        }
+
+        //-----Write all data to sql tables-------------------------------------
         ContentValues cv = new ContentValues();
         cv.put(KEY_USER_PK, userID);
-        cv.put(KEY_ACCOUNT_NAME, account.getName());
-        cv.put(KEY_ACCOUNT_BALANCE, account.getBalance().toPlainString());
+        cv.put(KEY_ACCOUNT_CLASS, account.getClass().getName());
+        cv.put(KEY_ACCOUNT_NAME, name);
+        cv.put(KEY_ACCOUNT_BALANCE, balance);
+        cv.put(KEY_ACCOUNT_EXTRAS, extras.toString());
 
         db.insert(TABLE_ACCOUNT, null, cv);
     }
@@ -166,19 +236,19 @@ public final class AndroidLocalDatabase extends SQLiteOpenHelper {
         //-----Get the standard transaction data, with appropriate defaults-----
         String amount = writeData.remove(Transaction.SAVE_KEY_AMOUNT);
         if (amount == null) {
-            System.out.println("Error getting amount from saved transaction data.");
+            System.err.println("Error getting amount from saved transaction data.");
             amount = "0";
         }
 
         String timeEntered = writeData.remove(Transaction.SAVE_KEY_TIME_ENTERED);
         if (timeEntered == null) {
-            System.out.println("Error getting time entered from saved transaction data.");
+            System.err.println("Error getting time entered from saved transaction data.");
             timeEntered = SimpleDateFormat.getDateTimeInstance().format(new Date());
         }
 
         String timeEffective = writeData.remove(Transaction.SAVE_KEY_TIME_EFFECTIVE);
         if (timeEffective == null) {
-            System.out.println("Error getting time effective from saved transaction data.");
+            System.err.println("Error getting time effective from saved transaction data.");
             timeEffective = SimpleDateFormat.getDateTimeInstance().format(new Date());
         }
 
@@ -212,9 +282,38 @@ public final class AndroidLocalDatabase extends SQLiteOpenHelper {
         List<User> users = new ArrayList<User>(c.getCount());
         for (int i = 0; i < c.getCount(); i++) {
             c.moveToNext();
-            String username = c.getString(c.getColumnIndex(KEY_USER_USERNAME));
-            String password = c.getString(c.getColumnIndex(KEY_USER_PASSWORD));
-            users.add(new User(username, password));
+
+            Map<String, String> readData = new HashMap<String, String>();
+            String className = c.getString(c.getColumnIndex(KEY_USER_CLASS));
+
+            readData.put(User.SAVE_KEY_USERNAME, c.getString(c.getColumnIndex(KEY_USER_USERNAME)));
+            readData.put(User.SAVE_KEY_PASSWORD, c.getString(c.getColumnIndex(KEY_USER_PASSWORD)));
+
+            String extras = c.getString(c.getColumnIndex(KEY_USER_EXTRAS));
+            for (String extra: extras.split("\\|")) {
+                if (extra.equals("")) continue; // Skip empty entries
+                int splitIndex = extra.indexOf(":");
+                readData.put(extra.substring(0, splitIndex), extra.substring(splitIndex + 1));
+            }
+
+            // Create the user
+            User user = null;
+            try {
+                @SuppressWarnings("unchecked")
+                Class<? extends User> userClass = (Class<? extends User>) Class.forName(className);
+                user = userClass.newInstance();
+                user.read(readData);
+            } catch (ClassNotFoundException e) {
+                System.err.println("Error finding user class name: " + className + ".");
+            } catch (InstantiationException e) {
+                System.err.println("Error creating user with class name: " + className + ".");
+            } catch (IllegalAccessException e) {
+                System.err.println("Error creating user with class name: " + className + ".");
+            }
+
+            if (user != null) {
+                users.add(user);
+            }
         }
 
         return users;
@@ -237,9 +336,38 @@ public final class AndroidLocalDatabase extends SQLiteOpenHelper {
         List<Account> accounts = new ArrayList<Account>();
         for (int i = 0; i < c.getCount(); i++) {
             c.moveToNext();
-            String name = c.getString(c.getColumnIndex(KEY_ACCOUNT_NAME));
-            String balance = c.getString(c.getColumnIndex(KEY_ACCOUNT_BALANCE));
-            accounts.add(new Account(name, new BigDecimal(balance)));
+
+            Map<String, String> readData = new HashMap<String, String>();
+            String className = c.getString(c.getColumnIndex(KEY_ACCOUNT_CLASS));
+
+            readData.put(Account.SAVE_KEY_NAME, c.getString(c.getColumnIndex(KEY_ACCOUNT_NAME)));
+            readData.put(Account.SAVE_KEY_BALANCE, c.getString(c.getColumnIndex(KEY_ACCOUNT_BALANCE)));
+
+            String extras = c.getString(c.getColumnIndex(KEY_ACCOUNT_EXTRAS));
+            for (String extra: extras.split("\\|")) {
+                if (extra.equals("")) continue; // Skip empty entries
+                int splitIndex = extra.indexOf(":");
+                readData.put(extra.substring(0, splitIndex), extra.substring(splitIndex + 1));
+            }
+
+            // Create the account
+            Account account = null;
+            try {
+                @SuppressWarnings("unchecked")
+                Class<? extends Account> accountClass = (Class<? extends Account>) Class.forName(className);
+                account = accountClass.newInstance();
+                account.read(readData);
+            } catch (ClassNotFoundException e) {
+                System.err.println("Error finding account class name: " + className + ".");
+            } catch (InstantiationException e) {
+                System.err.println("Error creating account with class name: " + className + ".");
+            } catch (IllegalAccessException e) {
+                System.err.println("Error creating account with class name: " + className + ".");
+            }
+
+            if (account != null) {
+                accounts.add(account);
+            }
         }
 
         return accounts;
@@ -294,11 +422,11 @@ public final class AndroidLocalDatabase extends SQLiteOpenHelper {
                 transaction = transactionClass.newInstance();
                 transaction.read(readData);
             } catch (ClassNotFoundException e) {
-                System.out.println("Error finding transaction class name: " + className + ".");
+                System.err.println("Error finding transaction class name: " + className + ".");
             } catch (InstantiationException e) {
-                System.out.println("Error creating transaction with class name: " + className + ".");
+                System.err.println("Error creating transaction with class name: " + className + ".");
             } catch (IllegalAccessException e) {
-                System.out.println("Error creating transaction with class name: " + className + ".");
+                System.err.println("Error creating transaction with class name: " + className + ".");
             }
 
             if (transaction != null) {
