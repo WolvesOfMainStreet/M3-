f061e4a19f6819e4d3b39c324a37f8caccdac2fd
DNoved1 <dnoved1@aim.com>
2014-03-14 13:54:49 -0400

    Javadocs for screen interfaces

--- src/main/java/cs2340/woms/view/screens/UserOverviewScreen.java
+++ src/main/java/cs2340/woms/view/screens/UserOverviewScreen.java
@@ -1,3 +1,7 @@
 package cs2340.woms.view.screens;
 
+/**
+ *  The user overview screen, which acts as a hub linking to all the other
+ *  user based screens.
+ */
 public interface UserOverviewScreen extends BaseScreen { }
