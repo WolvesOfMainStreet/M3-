60adf1751119529c27269ba87cb946e99afa5c6d
DNoved1 <dnoved1@aim.com>
2014-03-18 01:46:34 -0400

    Changed DatabaseConnection interface/use Futures
    All methods return Futures, allowing the use of database connections
    which may take longer than expected (like network based ones).
    
    Also allowed every method to throw IOException in the case of database
    access errors.
    Finally, removed IllegalArgumentException throws and merged the cases in
    which it could be thrown with IllegalStateException.

--- src/main/java/org/homenet/dnoved1/woms/model/DatabaseConnection.java
+++ src/main/java/org/homenet/dnoved1/woms/model/DatabaseConnection.java
@@ -1,6 +1,8 @@
 package org.homenet.dnoved1.woms.model;
 
+import java.io.IOException;
 import java.util.Collection;
+import java.util.concurrent.Future;
 
 import org.homenet.dnoved1.woms.model.report.Report;
 
@@ -11,66 +13,89 @@ import org.homenet.dnoved1.woms.model.report.Report;
 public interface DatabaseConnection {
 
     /**
-     * Attempts to login the given user, returning whether this was successful.
+     * Attempts to login the given user.
      *
      * @param user the user to login.
-     * @return true if the user was successfully logged in, false if they could
-     * not be.
+     * @return a future that returns true if the user was successfully logged
+     * in and false if they could not be.
+     * @throws IOException if some error occurs while while accessing the
+     * database.
      */
-    boolean login(User user);
+    Future<Boolean> login(User user)
+        throws IOException;
 
     /**
      * Logs out the currently logged in user, if any.
+     *
+     * @return a future that can be checked to determine when a user has been
+     * successfully logged out.
+     * @throws IOException if some error occurs while while accessing the
+     * database.
      */
-    void logout();
+    Future<Void> logout()
+        throws IOException;
 
     /**
-     * Attempts to register the given user. Returns whether this was
-     * successful.
+     * Attempts to register the given user.
      *
      * @param user the user to register.
-     * @return true if the user was successfully registered, false if not.
+     * @return a future that returns true if the user was successfully
+     * registered and false if they were not.
+     * @throws IOException if some error occurs while while accessing the
+     * database.
      */
-    boolean register(User user);
+    Future<Boolean> register(User user)
+        throws IOException;
 
     /**
      * Removes the currently logged in user from the database.
      *
+     * @return a future that can be checked to determine when a user has been
+     * successfully removed.
+     * @throws IOException if some error occurs while while accessing the
+     * database.
      * @throws IllegalStateException if no user is currently logged in.
      */
-    void removeUser()
-        throws IllegalStateException;
+    Future<Void> removeUser()
+        throws IOException, IllegalStateException;
 
     /**
      * Returns all of the currently logged in user's accounts.
      *
-     * @return the currently logged in user's accounts.
+     * @return a future that returns the currently logged in user's accounts.
+     * @throws IOException if some error occurs while while accessing the
+     * database.
      * @throws IllegalStateException if no user is currently logged in.
      */
-    Collection<Account> getAllAccounts()
-        throws IllegalStateException;
+    Future<Collection<Account>> getAllAccounts()
+        throws IOException, IllegalStateException;
 
     /**
      * Returns all of the currently logged in user's transactions for the given
      * account.
      *
      * @param account the account to get transactions for.
-     * @return all of the given account's transactions.
-     * @throws IllegalStateException if no user is currently logged in.
-     * @throws IllegalArgumentException if the current user does not own the
-     * given account.
+     * @return a future that returns all of the given account's transactions.
+     * @throws IOException if some error occurs while while accessing the
+     * database.
+     * @throws IllegalStateException if no user is currently logged in or the
+     * logged in user does not own the given account.
      */
-    Collection<Transaction> getAllTransactions(Account account)
-        throws IllegalStateException, IllegalArgumentException;
+    Future<Collection<Transaction>> getAllTransactions(Account account)
+        throws IOException, IllegalStateException;
 
     /**
      * Adds the given account to the currently logged in user's accounts.
      *
      * @param account the account to add.
+     * @return a future that can be checked to determine when the account has
+     * been successfully added.
+     * @throws IOException if some error occurs while while accessing the
+     * database.
      * @throws IllegalStateException if no user is currently logged in.
      */
-    void addAccount(Account account)
-        throws IllegalStateException;
+    Future<Void> addAccount(Account account)
+        throws IOException, IllegalStateException;
 
     /**
      * Adds the given transaction to the given account belonging to the
@@ -78,23 +103,29 @@ public interface DatabaseConnection {
      *
      * @param account the account to add the transaction to.
      * @param transaction the transaction to add.
-     * @throws IllegalStateException if no user is currently logged in.
-     * @throws IllegalArgumentException if the current user does not own the
-     * given account.
+     * @return a future that can be checked to determine when the transaction
+     * has been successfully added.
+     * @throws IOException if some error occurs while while accessing the
+     * database.
+     * @throws IllegalStateException if no user is currently logged in or the
+     * logged in user does not own the given account.
      */
-    void addTransaction(Account account, Transaction transaction)
-        throws IllegalStateException, IllegalArgumentException;
+    Future<Void> addTransaction(Account account, Transaction transaction)
+        throws IOException, IllegalStateException;
 
     /**
      * Removes the given account from the currently logged in user's accounts.
      *
      * @param account the account to remove.
-     * @throws IllegalStateException if no user is currently logged in.
-     * @throws IllegalArgumentException if the current user does not own the
-     * given account.
+     * @return a future that can be checked to determine when the account has
+     * been successfully removed.
+     * @throws IOException if some error occurs while while accessing the
+     * database.
+     * @throws IllegalStateException if no user is currently logged in or the
+     * logged in user does not own the given account.
      */
-    void removeAccount(Account account)
-        throws IllegalStateException, IllegalArgumentException;
+    Future<Void> removeAccount(Account account)
+        throws IOException, IllegalStateException;
 
     /**
      * Removes the given transaction from the given account belonging to the
@@ -102,12 +133,16 @@ public interface DatabaseConnection {
      *
      * @param account the account to remove the transaction from.
      * @param transaction the transaction to add.
-     * @throws IllegalStateException if no user is currently logged in.
-     * @throws IllegalArgumentException if the current user does not own the
-     * given account or the given account does not have the given transaction.
+     * @return a future that can be checked to determine when the transaction
+     * has been successfully removed.
+     * @throws IOException if some error occurs while while accessing the
+     * database.
+     * @throws IllegalStateException if no user is currently logged in, the
+     * logged in user does not own the given account, or the given account does
+     * not have the given transaction.
      */
-    void removeTransaction(Account account, Transaction transaction)
-        throws IllegalStateException, IllegalArgumentException;
+    Future<Void> removeTransaction(Account account, Transaction transaction)
+        throws IOException, IllegalStateException;
 
     /**
      * Updates the currently logged in user's information to match the given
@@ -115,10 +150,14 @@ public interface DatabaseConnection {
      *
      * @param nuw a user object containing the information the current user
      * should match.
+     * @return a future that can be checked to determine when the user has
+     * been successfully updated.
+     * @throws IOException if some error occurs while while accessing the
+     * database.
      * @throws IllegalStateException if no user is currently logged in.
      */
-    void updateUser(User nuw)
-        throws IllegalStateException;
+    Future<Void> updateUser(User nuw)
+        throws IOException, IllegalStateException;
 
     /**
      * Updates the old account's information to match the new account's
@@ -128,12 +167,15 @@ public interface DatabaseConnection {
      * @param old the old account to update.
      * @param nuw an account object containing the new information for the
      * account.
-     * @throws IllegalStateException if no user is currently logged in.
-     * @throws IllegalArgumentException if the current user does not own the
-     * given account.
+     * @return a future that can be checked to determine when the account has
+     * been successfully updated.
+     * @throws IOException if some error occurs while while accessing the
+     * database.
+     * @throws IllegalStateException if no user is currently logged in or the
+     * logged in user does not own the given account.
      */
-    void updateAccount(Account old, Account nuw)
-        throws IllegalStateException, IllegalArgumentException;
+    Future<Void> updateAccount(Account old, Account nuw)
+        throws IOException, IllegalStateException;
 
     /**
      * Updates the old transactions' information to match the new transactions'
@@ -144,18 +186,26 @@ public interface DatabaseConnection {
      * @param old the old transaction to update.
      * @param nuw a transaction object containing the new information for the
      * transaction.
-     * @throws IllegalStateException if no user is currently logged in.
-     * @throws IllegalArgumentException if the current user does not own the
-     * given account or the given account does not have the given transaction.
+     * @return a future that can be checked to determine when the transaction
+     * has been successfully updated.
+     * @throws IOException if some error occurs while while accessing the
+     * database.
+     * @throws IllegalStateException if no user is currently logged in, the
+     * logged in user does not own the given account, or the given account does
+     * not have the given transaction.
      */
-    void updateTransaction(Account account, Transaction old, Transaction nuw)
-        throws IllegalStateException, IllegalArgumentException;
+    Future<Void> updateTransaction(Account account, Transaction old, Transaction nuw)
+        throws IOException, IllegalStateException;
 
     /**
      * Makes the given report visit every element of this database.
      *
      * @param report the report to accept.
-     * @return the report after it has visited every element.
+     * @return a future that returns the report after it has visited every
+     * element.
+     * @throws IOException if some error occurs while while accessing the
+     * database.
      */
-    Report accept(Report report);
+    Future<Report> accept(Report report)
+        throws IOException;
 }
